1858e74c57ea3466565253b3ea89552ab821de5d
黃恩浩 howardhuang <I11101001@pershing.com.tw>
2022-04-14 20:24:06 +0800

    MongoCompass to create database and connect

--- server_99.js
+++ server_99.js
@@ -6,10 +6,27 @@ import express from 'express';
 
 const app =express();
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import connectDB from './db/connect_99.js'
+
 app.get('/',(req,res)=>{
     res.send('Wellcome Enhao 209410199');
 });
 
 const port =process.env.PORT || 5000;
 
-app.listen(port,()=>console.log(`Server is running on port ${port}`));
+const start = async()=>{
+    try{
+        await connectDB(process.env.MONGO_LOCAL_URL).then( ()=>{
+            console.log('Connecting to MongoDB');
+        });
+        app.listen(port,()=>console.log(`Server is running on port ${port}`));
+    }catch{
+        console.log(err);
+    }
+}
+
+start();
+
